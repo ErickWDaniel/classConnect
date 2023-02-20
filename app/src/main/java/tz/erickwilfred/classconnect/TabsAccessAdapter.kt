cdd2eb4ce91d8fd721e3db5c6db13dd746c22ea7
ErickWDaniel <67169390+ErickWDaniel@users.noreply.github.com>
2023-02-20 11:59:23 +0300

    Initial commit

---
+++ app/src/main/java/tz/erickwilfred/classconnect/TabsAccessAdapter.kt
@@ -0,0 +1,15 @@
+package tz.erickwilfred.classconnect
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class TabsAccessAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
